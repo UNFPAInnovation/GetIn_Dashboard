c700bdb3c8aaad596a1fa28c02c13d77e836c4a7
ewinkler <winkler.em@gmail.com>
2018-10-09 21:14:02 +0000

    Add include files with common js

---
+++ includes/datatablejs.php
@@ -0,0 +1,6 @@
+<!-- data table javascript -->
+<script src="./js/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="./js/plugins/datatables/DT_bootstrap.js"></script>
+<script src="./js/plugins/tableCheckable/jquery.tableCheckable.js"></script>
+
+<script src="./js/plugins/icheck/jquery.icheck.min.js"></script>
---
+++ includes/footerjs.php
@@ -0,0 +1,4 @@
+<!-- javascript to load after footer -->
+<script src="./js/libs/jquery-1.9.1.min.js"></script>
+<script src="./js/libs/jquery-ui-1.9.2.custom.min.js"></script>
+<script src="./js/libs/bootstrap.min.js"></script>
